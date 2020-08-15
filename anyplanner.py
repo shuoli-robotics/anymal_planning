dcdacdadaf8cb87ac5ed71d07b8eb55148a39838
Shuo Li <ls90911@gmail.com>
2020-08-15 21:11:02 +0100

    four foot planning is done

--- anyplanner.py
+++ anyplanner.py
@@ -27,49 +27,114 @@ class AnyPlanner:
         states = ["x","y","z","theta"]
         footstepStatesUpperBounds = {}
         footstepStatesLowerBounds = {}
-        footstepStates = model.addVars(steps,states,name="footstepStates")
 
-        footstepAssignment = model.addVars(steps,planes,name="footstepAssignment",vtype=GRB.BINARY)
+        footstepStates = model.addVars(steps,footIds,states,name="footstepStates")
 
-        constOnePlane = model.addConstrs((quicksum(footstepAssignment[step,plane] for plane in planes) == 1 for step in steps),name='constOnePlane')
+        footstepAssignment = model.addVars(steps,footIds,planes,name="footstepAssignment",vtype=GRB.BINARY)
+
+
+        # Assign each foothold on one plane
+        constOnePlane = model.addConstrs((quicksum(footstepAssignment[step,footId,plane] for plane in planes) == 1 for step in steps for footId in footIds),name='constOnePlane')
         
-        constOnThePlane0 = model.addConstrs(( (footstepAssignment[step,plane]==1) >>(planesCoef[plane,0]*footstepStates[step,'x']+planesCoef[plane,1]*footstepStates[step,'y']+planesCoef[plane,2]*footstepStates[step,'z']+planesCoef[plane,3] == 0) 
-                                     for step in steps for plane in planes) ,name='constOnThePlane')
-        constOnThePlane1 = model.addConstrs((footstepAssignment[step,plane]==1) >> (footstepStates[step,"x"] <= planesCoef[plane,5]) for step in steps for plane in planes)
-        constOnThePlane2 = model.addConstrs((footstepAssignment[step,plane]==1) >> (footstepStates[step,"x"] >= planesCoef[plane,4]) for step in steps for plane in planes)
-        constOnThePlane3 = model.addConstrs((footstepAssignment[step,plane]==1) >> (footstepStates[step,"y"] <= planesCoef[plane,7]) for step in steps for plane in planes)
-        constOnThePlane4 = model.addConstrs((footstepAssignment[step,plane]==1) >> (footstepStates[step,"y"] >= planesCoef[plane,6]) for step in steps for plane in planes)
+        constOnThePlane0 = model.addConstrs(( (footstepAssignment[step,footId,plane]==1) >>(planesCoef[plane,0]*footstepStates[step,footId,'x']+planesCoef[plane,1]*footstepStates[step,footId,'y']+planesCoef[plane,2]*footstepStates[step,footId,'z']+planesCoef[plane,3] == 0) 
+                                     for step in steps for footId in footIds for plane in planes ) ,name='constOnThePlane')
+        constOnThePlane1 = model.addConstrs((footstepAssignment[step,footId,plane]==1) >> (footstepStates[step,footId,"x"] <= planesCoef[plane,5]) for step in steps for footId in footIds for plane in planes)
+        constOnThePlane2 = model.addConstrs((footstepAssignment[step,footId,plane]==1) >> (footstepStates[step,footId,"x"] >= planesCoef[plane,4]) for step in steps for footId in footIds for plane in planes)
+        constOnThePlane3 = model.addConstrs((footstepAssignment[step,footId,plane]==1) >> (footstepStates[step,footId,"y"] <= planesCoef[plane,7]) for step in steps for footId in footIds for plane in planes)
+        constOnThePlane4 = model.addConstrs((footstepAssignment[step,footId,plane]==1) >> (footstepStates[step,footId,"y"] >= planesCoef[plane,6]) for step in steps for footId in footIds for plane in planes)
         
+        # Limit the distance between each step for each leg
         maxDistanceBetweenSteps = 0.4
-        model.addConstrs((footstepStates[step,state]-footstepStates[step-1,state] <= maxDistanceBetweenSteps for step in steps for state in states if step !=steps[0]),name="stepLimit")
-        model.addConstrs((footstepStates[step,state]-footstepStates[step-1,state] >= -maxDistanceBetweenSteps for step in steps for state in states if step !=steps[0]),name="stepLimit")
+        model.addConstrs((footstepStates[step,footId,state]-footstepStates[step-1,footId,state] <= maxDistanceBetweenSteps for step in steps for footId in footIds for state in states if step !=steps[0]),name="stepLimit")
+        model.addConstrs((footstepStates[step,footId,state]-footstepStates[step-1,footId,state] >= -maxDistanceBetweenSteps for step in steps for footId in footIds for state in states if step !=steps[0]),name="stepLimit")
+
+        # Limit the distance between the legs for each step
+        minDistanceBetweenLeftAndRight = 0.1
+        maxDistanceBetweenLeftAndRight = 0.2
+        minDistanceBetweenFrontAndHind= 0.2
+        maxDistanceBetweenFrontAndHind= 0.6
+        model.addConstrs((footstepStates[step,footIds[1],"x"] - footstepStates[step,footIds[0],"x"] >= minDistanceBetweenLeftAndRight for step in steps), name = "RF limit")
+        model.addConstrs((footstepStates[step,footIds[1],"x"] - footstepStates[step,footIds[0],"x"] <=  maxDistanceBetweenLeftAndRight for step in steps), name = "RF limit")
+        model.addConstrs((footstepStates[step,footIds[1],"y"] - footstepStates[step,footIds[0],"y"] >= -maxDistanceBetweenLeftAndRight for step in steps), name = "RF limit")
+        model.addConstrs((footstepStates[step,footIds[1],"y"] - footstepStates[step,footIds[0],"y"] <=  maxDistanceBetweenLeftAndRight for step in steps), name = "RF limit")
+
+        model.addConstrs((footstepStates[step,footIds[2],"x"] - footstepStates[step,footIds[0],"x"] <=  maxDistanceBetweenLeftAndRight for step in steps), name = "LH limit")
+        model.addConstrs((footstepStates[step,footIds[2],"x"] - footstepStates[step,footIds[0],"x"] >=  -maxDistanceBetweenLeftAndRight for step in steps), name = "LH limit")
+        model.addConstrs((footstepStates[step,footIds[0],"y"] - footstepStates[step,footIds[2],"y"] <=  maxDistanceBetweenFrontAndHind for step in steps), name = "LH limit")
+        model.addConstrs((footstepStates[step,footIds[0],"y"] - footstepStates[step,footIds[2],"y"] >=  minDistanceBetweenFrontAndHind for step in steps), name = "LH limit")
+
+
+        model.addConstrs((footstepStates[step,footIds[3],"x"] - footstepStates[step,footIds[1],"x"] <=  maxDistanceBetweenLeftAndRight for step in steps), name = "RH limit")
+        model.addConstrs((footstepStates[step,footIds[3],"x"] - footstepStates[step,footIds[1],"x"] >=  -maxDistanceBetweenLeftAndRight for step in steps), name = "RH limit")
+        model.addConstrs((footstepStates[step,footIds[1],"y"] - footstepStates[step,footIds[3],"y"] <=  maxDistanceBetweenFrontAndHind for step in steps), name = "RH limit")
+        model.addConstrs((footstepStates[step,footIds[1],"y"] - footstepStates[step,footIds[3],"y"] >=  minDistanceBetweenFrontAndHind for step in steps), name = "RH limit")
+
+
+        # Initial states and final states
+        model.addConstrs(footstepStates[steps[0],footId,state] <= maxDistanceBetweenSteps for state in states for footId in footIds if state != states[-1])
+        model.addConstrs(footstepStates[steps[-1],footId,states[0]]-self.targetPoint[0] <= maxDistanceBetweenSteps for footId in footIds)
+        model.addConstrs(footstepStates[steps[-1],footId,states[0]]-self.targetPoint[0] >= -maxDistanceBetweenSteps for footId in footIds)
+        model.addConstrs(footstepStates[steps[-1],footId,states[1]]-self.targetPoint[1] <= maxDistanceBetweenSteps for footId in footIds)
+        model.addConstrs(footstepStates[steps[-1],footId,states[1]]-self.targetPoint[1] >= -maxDistanceBetweenSteps for footId in footIds)
+        model.addConstrs(footstepStates[steps[0],footId,states[-1]] == 0.0 for footId in footIds)
+        model.addConstrs(footstepStates[steps[-1],footId,states[-1]] == 0.0 for footId in footIds)
         
-        model.addConstrs(footstepStates[steps[0],state] <= maxDistanceBetweenSteps for state in states if state != states[-1])
-        model.addConstr(footstepStates[steps[-1],states[0]]-self.targetPoint[0] <= maxDistanceBetweenSteps)
-        model.addConstr(footstepStates[steps[-1],states[0]]-self.targetPoint[0] >= -maxDistanceBetweenSteps)
-        model.addConstr(footstepStates[steps[-1],states[1]]-self.targetPoint[1] <= maxDistanceBetweenSteps)
-        model.addConstr(footstepStates[steps[-1],states[1]]-self.targetPoint[1] >= -maxDistanceBetweenSteps)
-    
-        model.addConstr(footstepStates[steps[0],states[-1]] == 0.0)
-        model.addConstr(footstepStates[steps[-1],states[-1]] == 0.0)
-        
-        obj = quicksum((footstepStates[step,states[0]]-footstepStates[step-1,states[0]])*(footstepStates[step,states[0]]-footstepStates[step-1,states[0]])+(footstepStates[step,states[1]]-footstepStates[step-1,states[1]])*(footstepStates[step,states[1]]-footstepStates[step-1,states[1]])
-              for step in steps if step != steps[0])
+        obj = quicksum((footstepStates[step,footId,states[0]]-footstepStates[step-1,footId,states[0]])*(footstepStates[step,footId,states[0]]-footstepStates[step-1,footId,states[0]])+(footstepStates[step,footId,states[1]]-footstepStates[step-1,footId,states[1]])*(footstepStates[step,footId,states[1]]-footstepStates[step-1,footId,states[1]])
+              for step in steps for footId in footIds if step != steps[0])
         model.setObjective(obj, GRB.MINIMIZE)
         model.optimize()
         
-        optimalFootstep = np.zeros((3,N))
+        optimalFootstepLF = np.zeros((3,N))
+        optimalFootstepRF = np.zeros((3,N))
+        optimalFootstepLH = np.zeros((3,N))
+        optimalFootstepRH = np.zeros((3,N))
         solutions = model.getAttr('x', footstepStates)
-        #print(solutions)
+
+        print(solutions)
         for solu in solutions:
-            if solu[1] == 'x':
-                optimalFootstep[0,solu[0]] = solutions[solu]
-            elif solu[1] == 'y':
-                optimalFootstep[1,solu[0]] = solutions[solu]
-            elif solu[1] == 'z':
-                optimalFootstep[2,solu[0]] = solutions[solu]
-        self.ax.scatter(optimalFootstep[0,:],optimalFootstep[1,:],optimalFootstep[2,:])
-        self.set_axes_equal()
+            if solu[1] == 'LF':
+                if solu[2] == 'x':
+                    optimalFootstepLF[0,solu[0]] = solutions[solu]
+                elif solu[2] == 'y':
+                    optimalFootstepLF[1,solu[0]] = solutions[solu]
+                elif solu[2] == 'z':
+                    optimalFootstepLF[2,solu[0]] = solutions[solu]
+            elif solu[1] == 'RF':
+                if solu[2] == 'x':
+                    optimalFootstepRF[0,solu[0]] = solutions[solu]
+                elif solu[2] == 'y':
+                    optimalFootstepRF[1,solu[0]] = solutions[solu]
+                elif solu[2] == 'z':
+                    optimalFootstepRF[2,solu[0]] = solutions[solu]
+            elif solu[1] == 'LH':
+                if solu[2] == 'x':
+                    optimalFootstepLH[0,solu[0]] = solutions[solu]
+                elif solu[2] == 'y':
+                    optimalFootstepLH[1,solu[0]] = solutions[solu]
+                elif solu[2] == 'z':
+                    optimalFootstepLH[2,solu[0]] = solutions[solu]
+            elif solu[1] == 'RH':
+                if solu[2] == 'x':
+                    optimalFootstepRH[0,solu[0]] = solutions[solu]
+                elif solu[2] == 'y':
+                    optimalFootstepRH[1,solu[0]] = solutions[solu]
+                elif solu[2] == 'z':
+                    optimalFootstepRH[2,solu[0]] = solutions[solu]
+
+        print(optimalFootstepLF[:,0:5])
+        print(optimalFootstepRF[:,0:5])
+        print(optimalFootstepLH[:,0:5])
+        print(optimalFootstepRH[:,0:5])
+
+        self.ax.scatter(optimalFootstepLF[0,:],optimalFootstepLF[1,:],optimalFootstepLF[2,:],color = 'white')
+        self.ax.scatter(optimalFootstepRF[0,:],optimalFootstepRF[1,:],optimalFootstepRF[2,:],color = 'red')
+        self.ax.scatter(optimalFootstepLH[0,:],optimalFootstepLH[1,:],optimalFootstepLH[2,:],color = 'yellow')
+        self.ax.scatter(optimalFootstepRH[0,:],optimalFootstepRH[1,:],optimalFootstepRH[2,:],color = 'green')
+
+        self.ax.set_xlim([0,10])
+        self.ax.set_ylim([0,20])
+        self.ax.set_zlim([0,5])
+        #self.set_axes_equal()
         
     def set_axes_equal(self):
         x_limits = self.ax.get_xlim3d()
