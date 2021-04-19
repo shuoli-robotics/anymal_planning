9aa1b9bee0f857f2c00b630abfbcba9cdabfedc9
Shuo Li <ls90911@gmail.com>
2021-04-19 11:00:22 +0100

    can successfully publish trajectory reference to the Anymal simulator and the Anymal does correspond. However, the Anymal is not stable

--- any_node_listener.py
+++ any_node_listener.py
@@ -1,64 +1,22 @@
 # !/usr/bin/env python
 # import roslib
 import rospy
-from std_msgs.msg import String
-from geometry_msgs.msg import TwistWithCovarianceStamped
-from signal_logger_msgs.msg import BoolStamped
-from geometry_msgs.msg import Vector3Stamped
-from std_msgs.msg import Bool
-from geometry_msgs.msg import QuaternionStamped
-from anymal_msgs.msg import AnymalState
 import astar_ros
-
-def listener():
-    rospy.init_node('anyplan_listener')
-
-    rospy.Subscriber("/log/loco/whole_body/positionWorldToComInWorldFrame", Vector3Stamped, astar.set_center_of_mass_callback)
-    rospy.Subscriber("/log/loco/whole_body/linearVelocityComInWorldFrame", Vector3Stamped, astar.set_velocity_of_mass_callback)
-    rospy.Subscriber("/log/loco/torso/measured/orientationWorldToControl",QuaternionStamped, astar.set_orientation_callback)
-    rospy.Subscriber("/log/loco/leftFore/isGrounded", BoolStamped, astar.set_on_ground_LF_callback)
-    rospy.Subscriber("/log/loco/rightFore/isGrounded", BoolStamped, astar.set_on_ground_RF_callback)
-    rospy.Subscriber("/log/loco/leftHind/isGrounded", BoolStamped, astar.set_on_ground_LH_callback)
-    rospy.Subscriber("/log/loco/rightHind/isGrounded", BoolStamped, astar.set_on_ground_RH_callback)
-    rospy.Subscriber("/log/loco/leftFore/positionWorldToEEOriginInWorldFrame", Vector3Stamped, astar.set_EE_LF_callback)
-    rospy.Subscriber("/log/loco/rightFore/positionWorldToEEOriginInWorldFrame", Vector3Stamped, astar.set_EE_RF_callback)
-    rospy.Subscriber("/log/loco/leftHind/positionWorldToEEOriginInWorldFrame", Vector3Stamped, astar.set_EE_LH_callback)
-    rospy.Subscriber("/log/loco/rightHind/positionWorldToEEOriginInWorldFrame", Vector3Stamped, astar.set_EE_RH_callback)
-    rospy.Subscriber("calc_zmp_cmd", Bool,astar.calc_zmp_callback)
-    rospy.Subscriber("set_goal", Vector3Stamped, astar.set_goal_callback)
-    rospy.Subscriber("astar_run", Vector3Stamped, astar.run_callback)
-    rospy.Subscriber("display_path", Bool, astar.display_path_callback)
-    rospy.Subscriber("display_path", Bool, astar.display_path_callback)
-
-    rospy.spin()
-
-    print("listener ends")
-
-def callback(data):
-    pass
-    # print(data.twist.twist.linear.x)
-
-
-# def talker():
-#     pub = rospy.Publisher('chatter', String, queue_size=10)
-#     rospy.init_node('talker', anonymous=True)
-#     rate = rospy.Rate(10) # 10hz
-#     while not rospy.is_shutdown():
-#         hello_str = "hello world %s" % rospy.get_time()
-#         rospy.loginfo(hello_str)
-#         pub.publish(hello_str)
-#         rate.sleep()
 
 if __name__ == '__main__':
     zmp_0 = (2.0,1.0,0.)
     zmp_f = (5.0,5,0)
     astar = astar_ros.AnymalAStarRos(zmp_0,zmp_f)
-    listener()
+    wait_time = 1
+    r_wait_rate = rospy.Rate(1/wait_time)
+    r_wait_rate.sleep()
+    astar.test_calc_lf_ee_traj_callback()
 
-    # astar.run()
+    r = rospy.Rate(200)
 
-    # try:
-    #     listener()
-    # except rospy.ROSInterruptException:
-    #     pass
+    while not rospy.is_shutdown():
+        astar.publish_whole_body_trajectory_reference()
+        r.sleep()
+
+
 
--- astar_ros.py
+++ astar_ros.py
@@ -1,9 +1,20 @@
 from astar_LIP import AnymalAStar
 import numpy as np
+from scipy.interpolate import CubicHermiteSpline
+from datetime import datetime
+
 import rospy
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
-from whole_body_control_msg.msg import whole_body_control_msg
+from anymal_ctrl_edin_gaits.msg import whole_body_control_msg
+
+from std_msgs.msg import String
+from geometry_msgs.msg import TwistWithCovarianceStamped
+from signal_logger_msgs.msg import BoolStamped
+from geometry_msgs.msg import Vector3Stamped
+from std_msgs.msg import Bool
+from geometry_msgs.msg import QuaternionStamped
+from anymal_msgs.msg import AnymalState
 
 class AnymalAStarRos(AnymalAStar):
     def __init__(self,zmp_0, zmp_f):
@@ -15,11 +26,30 @@ class AnymalAStarRos(AnymalAStar):
         # Anymal's feet states
         self.on_ground = [True,True,True,True] # LF, RF,LH, RH
 
+        self.trajectory_ready = False
+
         self.position_EE = np.zeros((4,3))
 
-        # rospy.init_node('AnymalAStar')
+        rospy.Subscriber("/log/loco/whole_body/positionWorldToComInWorldFrame", Vector3Stamped, self.set_center_of_mass_callback)
+        rospy.Subscriber("/log/loco/whole_body/linearVelocityComInWorldFrame", Vector3Stamped, self.set_velocity_of_mass_callback)
+        rospy.Subscriber("/log/loco/torso/measured/orientationWorldToControl", QuaternionStamped, self.set_orientation_callback)
+        rospy.Subscriber("/log/loco/leftFore/isGrounded", BoolStamped, self.set_on_ground_LF_callback)
+        rospy.Subscriber("/log/loco/rightFore/isGrounded", BoolStamped, self.set_on_ground_RF_callback)
+        rospy.Subscriber("/log/loco/leftHind/isGrounded", BoolStamped, self.set_on_ground_LH_callback)
+        rospy.Subscriber("/log/loco/rightHind/isGrounded", BoolStamped, self.set_on_ground_RH_callback)
+        rospy.Subscriber("/log/loco/leftFore/positionWorldToEEOriginInWorldFrame", Vector3Stamped, self.set_EE_LF_callback)
+        rospy.Subscriber("/log/loco/rightFore/positionWorldToEEOriginInWorldFrame", Vector3Stamped, self.set_EE_RF_callback)
+        rospy.Subscriber("/log/loco/leftHind/positionWorldToEEOriginInWorldFrame", Vector3Stamped, self.set_EE_LH_callback)
+        rospy.Subscriber("/log/loco/rightHind/positionWorldToEEOriginInWorldFrame", Vector3Stamped, self.set_EE_RH_callback)
+        rospy.Subscriber("calc_zmp_cmd", Bool, self.calc_zmp_callback)
+        rospy.Subscriber("set_goal", Vector3Stamped, self.set_goal_callback)
+        rospy.Subscriber("astar_run", Vector3Stamped, self.run_callback)
+        rospy.Subscriber("display_path", Bool, self.display_path_callback)
+
+        rospy.init_node('AnymalAStar')
         self.pub_LIP = rospy.Publisher("LIP", Marker, queue_size=10)
         self.pub_path = rospy.Publisher("optimal_path", Marker, queue_size=30)
+        self.pub_whole_body_ref = rospy.Publisher("/whole_body_control_reference", whole_body_control_msg, queue_size=30)
 
     def calc_zmp(self):
         contact_feet_position = []
@@ -86,7 +116,7 @@ class AnymalAStarRos(AnymalAStar):
         self.goal = [data.vector.x,data.vector.y,data.vector.z]
 
     def set_orientation_callback(self,data):
-        self.torso_orientation = [data.x,data.y,data.z,data.w]
+        self.torso_orientation = [data.quaternion.x,data.quaternion.y,data.quaternion.z,data.quaternion.w]
 
     def run_callback(self,data):
 
@@ -139,28 +169,103 @@ class AnymalAStarRos(AnymalAStar):
             # print("[astar_ros] display_path_callback p0 = {},p1 = {}".format(p0,p1))
             self.pub_path.publish(m)
 
-    def move_lf_ee_callback(self):
+    def test_calc_lf_ee_traj_callback(self):
+        self.traj_time = 2.0
+        time = [0,self.traj_time/2,self.traj_time]
+
+        lf_x_const = [self.position_EE[0,0],self.position_EE[0,0],self.position_EE[0,0]]
+        lf_y_const = [self.position_EE[0, 1], self.position_EE[0, 1], self.position_EE[0, 1]]
+        lf_z_const = [self.position_EE[0, 2],self.position_EE[0, 2]+0.08,self.position_EE[0, 2]]
+        lf_vel_const = [.0,.0,.0]
+
+        rf_x_const = [self.position_EE[1,0],self.position_EE[1,0],self.position_EE[1,0]]
+        rf_y_const = [self.position_EE[1, 1], self.position_EE[1, 1], self.position_EE[1, 1]]
+        rf_z_const = [self.position_EE[1, 2],self.position_EE[1, 2],self.position_EE[1, 2]]
+        rf_vel_const = [.0,.0,.0]
+
+        lh_x_const = [self.position_EE[2,0],self.position_EE[2,0],self.position_EE[2,0]]
+        lh_y_const = [self.position_EE[2, 1], self.position_EE[2, 1], self.position_EE[2, 1]]
+        lh_z_const = [self.position_EE[2, 2],self.position_EE[2, 2],self.position_EE[2, 2]]
+        lh_vel_const = [.0,.0,.0]
+
+        rh_x_const = [self.position_EE[3,0],self.position_EE[3,0],self.position_EE[3,0]]
+        rh_y_const = [self.position_EE[3, 1], self.position_EE[3, 1], self.position_EE[3, 1]]
+        rh_z_const = [self.position_EE[3, 2],self.position_EE[3, 2],self.position_EE[3, 2]]
+        rh_vel_const = [.0,.0,.0]
+
+        mass_center_x_const = [self.mass_point_pos[0],self.mass_point_pos[0],self.mass_point_pos[0]]
+        mass_center_y_const = [self.mass_point_pos[1], self.mass_point_pos[1], self.mass_point_pos[1]]
+        mass_center_z_const = [self.mass_point_pos[2], self.mass_point_pos[2], self.mass_point_pos[2]]
+        mass_center_vel_const = [.0, .0, .0]
+
+        orientation_x_const = [self.torso_orientation[0],self.torso_orientation[0],self.torso_orientation[0]]
+        orientation_y_const = [self.torso_orientation[1], self.torso_orientation[1], self.torso_orientation[1]]
+        orientation_z_const = [self.torso_orientation[2], self.torso_orientation[2], self.torso_orientation[2]]
+        orientation_w_const = [self.torso_orientation[3], self.torso_orientation[3], self.torso_orientation[3]]
+        orientation_vel_const = [.0, .0, .0]
+
+        self.lf_x_trajectory = CubicHermiteSpline(time, lf_x_const, lf_vel_const)
+        self.lf_y_trajectory = CubicHermiteSpline(time, lf_y_const, lf_vel_const)
+        self.lf_z_trajectory = CubicHermiteSpline(time, lf_z_const, lf_vel_const)
+        self.lf_vx_trajectory = self.lf_x_trajectory.derivative()
+        self.lf_vy_trajectory = self.lf_y_trajectory.derivative()
+        self.lf_vz_trajectory = self.lf_z_trajectory.derivative()
+
+
+        self.rf_x_trajectory = CubicHermiteSpline(time, rf_x_const, rf_vel_const)
+        self.rf_y_trajectory = CubicHermiteSpline(time, rf_y_const, rf_vel_const)
+        self.rf_z_trajectory = CubicHermiteSpline(time, rf_z_const, rf_vel_const)
+
+        self.lh_x_trajectory = CubicHermiteSpline(time, lh_x_const, lh_vel_const)
+        self.lh_y_trajectory = CubicHermiteSpline(time, lh_y_const, lh_vel_const)
+        self.lh_z_trajectory = CubicHermiteSpline(time, lh_z_const, lh_vel_const)
+
+        self.rh_x_trajectory = CubicHermiteSpline(time, rh_x_const, rh_vel_const)
+        self.rh_y_trajectory = CubicHermiteSpline(time, rh_y_const, rh_vel_const)
+        self.rh_z_trajectory = CubicHermiteSpline(time, rh_z_const, rh_vel_const)
+
+        self.mass_center_x_trajectory = CubicHermiteSpline(time, mass_center_x_const, mass_center_vel_const)
+        self.mass_center_y_trajectory = CubicHermiteSpline(time, mass_center_y_const, mass_center_vel_const)
+        self.mass_center_z_trajectory = CubicHermiteSpline(time, mass_center_z_const, mass_center_vel_const)
+
+        self.orientation_x_trajectory = CubicHermiteSpline(time, orientation_x_const, orientation_vel_const)
+        self.orientation_y_trajectory = CubicHermiteSpline(time, orientation_y_const, orientation_vel_const)
+        self.orientation_z_trajectory = CubicHermiteSpline(time, orientation_z_const, orientation_vel_const)
+        self.orientation_w_trajectory = CubicHermiteSpline(time, orientation_w_const, orientation_vel_const)
+
+
+        self.trajectory_ready = True
+        self.generated_trajectory_timestamp = datetime.now()
+
+    def publish_whole_body_trajectory_reference(self):
+
+        t = (datetime.now()-self.generated_trajectory_timestamp).total_seconds()
+
+        if t > self.traj_time:
+            return False
+
+
         traj_ref = whole_body_control_msg()
-        traj_ref.com_position.x = self.mass_point_pos[0]
-        traj_ref.com_position.y = self.mass_point_pos[1]
-        traj_ref.com_position.z = self.mass_point_pos[2]
+        traj_ref.com_position.x = self.mass_center_x_trajectory.__call__(t)
+        traj_ref.com_position.y = self.mass_center_y_trajectory.__call__(t)
+        traj_ref.com_position.z = self.mass_center_z_trajectory.__call__(t)
 
         traj_ref.base_angular_velocity.x = .0
         traj_ref.base_angular_velocity.y = .0
         traj_ref.base_angular_velocity.z = .0
 
-        traj_ref.base_orientation.x = 0.0
-        traj_ref.base_orientation.y = 0.0
-        traj_ref.base_orientation.z = 0.0
-        traj_ref.base_orientation.w = 1.0
+        traj_ref.base_orientation.x = self.orientation_x_trajectory.__call__(t)
+        traj_ref.base_orientation.y = self.orientation_y_trajectory.__call__(t)
+        traj_ref.base_orientation.z = self.orientation_z_trajectory.__call__(t)
+        traj_ref.base_orientation.w = self.orientation_w_trajectory.__call__(t)
 
-        traj_ref.foot1_position.x = self.position_EE[0, 0]
-        traj_ref.foot1_position.y = self.position_EE[0, 1]
-        traj_ref.foot1_position.z = self.position_EE[0, 2]
+        traj_ref.foot1_position.x = self.lf_x_trajectory.__call__(t)
+        traj_ref.foot1_position.y = self.lf_y_trajectory.__call__(t)
+        traj_ref.foot1_position.z = self.lf_z_trajectory.__call__(t)
 
-        traj_ref.foot1_velocity.x = 0
-        traj_ref.foot1_velocity.y = 0
-        traj_ref.foot1_velocity.z = 0
+        traj_ref.foot1_velocity.x = self.lf_vx_trajectory.__call__(t)
+        traj_ref.foot1_velocity.y = self.lf_vy_trajectory.__call__(t)
+        traj_ref.foot1_velocity.z = self.lf_vz_trajectory.__call__(t)
 
         traj_ref.foot1_isContact = False
 
@@ -168,6 +273,7 @@ class AnymalAStarRos(AnymalAStar):
         traj_ref.foot2_position.y = self.position_EE[1, 1]
         traj_ref.foot2_position.z = self.position_EE[1, 2]
 
+        # TODO
         traj_ref.foot2_velocity.x = 0
         traj_ref.foot2_velocity.y = 0
         traj_ref.foot2_velocity.z = 0
@@ -178,12 +284,14 @@ class AnymalAStarRos(AnymalAStar):
         traj_ref.foot3_position.y = self.position_EE[2, 1]
         traj_ref.foot3_position.z = self.position_EE[2, 2]
 
+        # TODO
         traj_ref.foot3_velocity.x = 0
         traj_ref.foot3_velocity.y = 0
         traj_ref.foot3_velocity.z = 0
 
         traj_ref.foot3_isContact = True
 
+        # TODO
         traj_ref.foot4_position.x = self.position_EE[2, 0]
         traj_ref.foot4_position.y = self.position_EE[2, 1]
         traj_ref.foot4_position.z = self.position_EE[2, 2]
@@ -193,6 +301,11 @@ class AnymalAStarRos(AnymalAStar):
         traj_ref.foot4_velocity.z = 0
 
         traj_ref.foot4_isContact = True
+
+        self.pub_whole_body_ref.publish(traj_ref)
+
+
+
 
 
 
